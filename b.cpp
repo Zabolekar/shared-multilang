e4cb2b177376c1489cb88c9a1c9039435b5b3496
Zabolekar <syekirin@gmail.com>
2023-02-12 11:27:58 +0100

    std::iota instead of loop

--- b.cpp
+++ b.cpp
@@ -1,10 +1,10 @@
 #include <cstddef>
+#include <numeric>
 
 extern "C"
 {
     void b(size_t n, int* buf)
     {
-        for (size_t i = 0; i < n; i++)
-            buf[i] = i + 3;
+        std::iota(buf, buf + n, 3);
     }
 }
