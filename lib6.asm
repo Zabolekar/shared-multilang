bb50b7b0bf4df2b050391a4635d8f0b6af7025bb
Zabolekar <syekirin@gmail.com>
2023-02-13 15:15:08 +0100

    formatting

--- lib6.asm
+++ lib6.asm
@@ -3,14 +3,14 @@
 section .text
 global f:function
 f:
-    mov eax, 5
+    mov     eax, 5
 loop:
-    test rdi, rdi
-    je done
-    mov dword [rsi], eax
-    add rsi, 4
-    inc eax
-    dec rdi
-    jmp loop
+    test    rdi, rdi
+    je      done
+    mov     dword [rsi], eax
+    add     rsi, 4
+    inc     eax
+    dec     rdi
+    jmp     loop
 done:
     ret
